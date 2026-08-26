b4f1a210e8cde8610be755cb3d9ac0cd45f5add6
ellietownsend <epaigetownsend@gmail.com>
2026-08-26 08:41:14 -0700

    add route to accept post request for notes upload

--- app/app.py
+++ app/app.py
@@ -6,23 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html', message="This data came from Python!")
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
-"""
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    user_message = ""
-    
-    # Check if the user submitted the form
-    if request.method == 'POST':
-        # Grab data from the HTML input field using its 'name' attribute
-        username = request.form.get('username')
-        user_message = f"Hello, {username}! Your frontend is officially connected to Flask."
-    
-    # Send the HTML template back, along with an optional message variable
-    return render_template('index.html', message=user_message)
+@app.route("/upload_notes", methods=['GET', 'POST'])
+def upload_notes():
+    return "<p>Getting sent to python!</p>"
 
 if __name__ == '__main__':
     app.run(debug=True)
-"""
