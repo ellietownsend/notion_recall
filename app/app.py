b1d3ddd15cccc3d3245eb799f35b56e55b16dc29
ellietownsend <epaigetownsend@gmail.com>
2026-08-26 09:08:39 -0700

    POST and GET request validated for /upload_notes route

--- app/app.py
+++ app/app.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html', message="This data came from Python!")
+    return render_template('index.html', message="Welcome to notion recall!")
 
 @app.route("/upload_notes", methods=['GET', 'POST'])
 def upload_notes():
-    return "<p>Getting sent to python!</p>"
+    if request.method == "POST":
+        notes = request.form.get("notes")
+        return f"""
+            <h1>Notes recieved: {notes} </h1>
+        """
+
+    return """
+    <form method = "POST" action = "">
+        <label>Upload a file of your notes:</label>
+            <input type = "file" 
+                    id = "notes" 
+                    name = "notes" 
+                    accept = "text/html, .html, .htm" />
+                <button type = "submit">Send notes off!</button>
+        </form>
+    """
 
 if __name__ == '__main__':
     app.run(debug=True)
